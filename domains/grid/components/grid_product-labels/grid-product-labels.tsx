250ab0bcc7ff05a90b49ae0b2d7d27c0bc367f61
Henri-Pierre RIGOULET <henri-pierre.rigoulet@latelier.co>
2023-07-19 10:24:32 +0200

    promo ok

--- domains/grid/components/grid_product-labels/grid-product-labels.tsx
+++ domains/grid/components/grid_product-labels/grid-product-labels.tsx
@@ -1,7 +1,9 @@
 // eslint-disable-next-line unicorn/filename-case
 'use-client';
+import clsx from 'clsx';
 import useAddToCart from 'domains/cart/hooks/add-to-cart';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import { FC } from 'react';
 import logo from '../../../../public/images/diver/AjouterPanier.png';
 import styles from './grid-product-labels.module.scss';
@@ -11,19 +13,21 @@ interface Props {
 
 const GridProductLabels: FC<Props> = ({ product }) => {
   const { addToCart } = useAddToCart(product?.variants?.[0]?.id);
+  const pathname = usePathname();
+  const isPromo = product?.tags?.find((tag) => tag === 'promotions') && pathname !== '/promotions';
 
   return product ? (
     <div>
-      <h3 data-testid='product-name' className={styles.title}>
+      <h3 data-testid="product-name" className={styles.title}>
         {product.title}
       </h3>
       <div className={styles.containerPrice}>
-        <div className={styles.price}>
+        <div className={clsx(styles.price, isPromo && styles.promo)}>
           {parseFloat(product.priceRange.minVariantPrice.amount)} €
         </div>
         <button className={styles.buttonAddToCart} onClick={addToCart}>
           {' '}
-          <Image src={logo} alt='panier-ajout' />{' '}
+          <Image src={logo} alt="panier-ajout" />{' '}
         </button>
       </div>
     </div>
