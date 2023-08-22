088c52cad979c2f3e82c057ec633bbf33541e808
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2023-08-22 15:02:53 +0200

    mage fixed

--- domains/product/components/gallery/gallery.tsx
+++ domains/product/components/gallery/gallery.tsx
@@ -1,10 +1,10 @@
 'use client';
 import clsx from 'clsx';
-import ArrowLeftIcon from 'domains/icons/arrow-left';
 import Image from 'next/image';
 import { useState } from 'react';
+import flecheDroite from '../../../../public/images/diver/FlecheDroite.png';
+import flecheGauche from '../../../../public/images/diver/FlecheGauche.png';
 import styles from './gallery.module.scss';
-
 export function Gallery({
   images
 }: {
@@ -38,43 +38,17 @@ export function Gallery({
       )}
 
       {images.length > 1 ? (
-        <div>
-          <button aria-label="Previous product image" onClick={() => handleNavigate('previous')}>
-            <ArrowLeftIcon className="h-6" />
+        <div className={styles.fleches}>
+          <button aria-label='Previous product image' onClick={() => handleNavigate('previous')}>
+            <Image src={flecheGauche} width={30} height={30} alt={'fleche-gauche'}  priority={true} />
           </button>
           <button
-            aria-label="Next product image"
+            aria-label='Next product image'
             className={clsx(buttonClassName)}
             onClick={() => handleNavigate('next')}
           >
-            <ArrowLeftIcon />
+           <Image src={flecheDroite} width={30} height={30} alt={'fleche-droite'}  priority={true} />
           </button>
-        </div>
-      ) : null}
-
-      {images.length > 1 ? (
-        <div>
-          {images.map((image, index) => {
-            return (
-              <button
-                aria-label="Enlarge product image"
-                key={image.src}
-                onClick={() => setCurrentImage(index)}
-              >
-                <Image
-                  alt={image?.altText}
-                  src={image.src}
-                  width={725}
-                  height={320}
-                  loading="lazy"
-                  quality={90}
-                  placeholder="empty"
-                  onLoad={(e) => console.log(e)}
-                  onError={(e) => console.log(e)}
-                />
-              </button>
-            );
-          })}
         </div>
       ) : null}
     </div>
