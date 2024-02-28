6f85db0c6e6e368d047a9a86171153c59970e6c9
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2024-02-28 15:21:23 +0100

    cookies ok

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AcceuilAvantages from 'domains/page-accueuil/components/acceuil-avantages
 import Carrousel from 'domains/page-accueuil/components/carrousel/carrousel';
 import { getAllProducts } from 'lib/shopify';
 import { Suspense } from 'react';
-
 export const metadata = {
   metadataBase: new URL(`https://www.jr-boutique.fr`),
   alternates: {
@@ -11,11 +10,11 @@ export const metadata = {
   title: process.env.SITE_NAME,
   robots: {
     index: true,
-    follow: true,
+    follow: true
   },
   googleBot: {
     index: true,
-    follow: true,
+    follow: true
   },
   description:
     'JR Distribution vous offre la Franche-Comté en un clic. Vente en ligne de fromages, produits régionnaux... ',
@@ -34,6 +33,7 @@ export const metadata = {
 export default async function HomePage() {
   const bestSellingProducts = await getAllProducts({ sortKey: 'BEST_SELLING', first: 12 });
   const newProducts = await getAllProducts({ sortKey: 'CREATED_AT', reverse: true, first: 12 });
+
   return (
     <>
       <AcceuilAvantages />
